83589c18a61cf2e99b02d274c3fbe36a509f70ce
mukundoff07 <66546011+mukundoff07@users.noreply.github.com>
2020-09-30 22:08:24 +0530

    Create sample_window.py

---
+++ sample_window.py
@@ -0,0 +1,18 @@
+# This Will Create A Sample Desktop Window With Tkinter 
+
+from tkinter import *  
+
+
+root = Tk()
+
+
+
+# Code To Edit The Title Of Desktop Window
+
+
+root.title("Sample Tkinter Window")
+
+
+
+
+root.mainloop()
